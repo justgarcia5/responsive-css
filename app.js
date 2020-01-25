fc8de807a2561d64b82345a48b3fe9dba4229aae
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-01-25 14:41:22 -0800

    css changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+// modile
 
 // accordion logic for mobile container
 let acc = document.getElementsByClassName("accordion");
-
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function() {
     this.classList.toggle('active');
@@ -14,20 +14,18 @@ for (let i = 0; i < acc.length; i++) {
   });
 }
 
-// logic to switch tabs inside accordion for mobile container
+// function to switch tabs inside accordion for mobile container
 function openTab(tab) {
-  let i;
   let x = document.getElementsByClassName('tab');
-  for (i = 0; i < x.length; i++) {
+  for (let i = 0; i < x.length; i++) {
     x[i].style.display = 'none';
   }
   document.getElementById(tab).style.display = 'block';
 }
 
-// logic for active tabs for inner accordion mobile view
+// function for active tabs for inner accordion mobile view
 let accTabsContainer = document.getElementById("tabs");
 let accTab = accTabsContainer.getElementsByClassName("acc-tab");
-
 for (let i = 0; i < accTab.length; i++) {
   accTab[i].addEventListener("click", function() {
     let current = document.getElementsByClassName("acc-tab active");
@@ -36,24 +34,46 @@ for (let i = 0; i < accTab.length; i++) {
   });
 }
 
+// desktop
 
+// function for switching tabs on desktop view
 function openDeskTab(tabId) {
-  let x;
   let tabs = document.getElementsByClassName("desk-tab");
-  for (x = 0; x < tabs.length; x++) {
+  for (let x = 0; x < tabs.length; x++) {
     tabs[x].style.display = "none";
   }
   document.getElementById(tabId).style.display = "block";
 }
 
-
+// function for opening and switching tabs inside DeskTap
 function openInnerTab(tabId) {
-  let x;
-  let innerTabs = document.getElementsByClassName("inner-tab");
-  // console.log(innerTabs)
-
-  for (x = 0; x < innerTabs.length; x++) {
+  let innerTabs = document.getElementsByClassName("tab-desk");
+  for (let x = 0; x < innerTabs.length; x++) {
     innerTabs[x].style.display = "none";
   }
   document.getElementById(tabId).style.display = "block";
+}
+
+// switching and replacing active class name inside DeskTab
+let innerTabsContainer = document.getElementById('inner-tab-links-div');
+let tabs = innerTabsContainer.getElementsByClassName('inner-tab-link');
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("inner-tab-link active");
+    current[0].className = current[0].className.replace(" active", "");
+    console.log(current[1])
+    this.className += " active";
+  });
+}
+
+// switching and replacing active class name for DeskTab
+let deskTabsContainer = document.getElementById('tab-links-div');
+let deskTabs = deskTabsContainer.getElementsByClassName('tab-link');
+for (let i = 0; i < deskTabs.length; i++) {
+  deskTabs[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName('tab-link active');
+    current[0].className = current[0].className.replace(" active", "");
+    console.log(current[1])
+    this.className += " active";
+  });
 }
