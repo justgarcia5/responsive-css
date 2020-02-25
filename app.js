3f6dda79ba3d7d98a471c6e77187592e93fbafd3
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-02-25 14:38:07 -0800

    made small change to js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// modile
 
 // accordion logic for mobile container
 let acc = document.getElementsByClassName("accordion");
@@ -14,16 +13,17 @@ for (let i = 0; i < acc.length; i++) {
   });
 }
 
-// function to switch tabs inside accordion for mobile container
-function openTab(tab) {
-  let x = document.getElementsByClassName('tab');
+// function to switch tabs
+function openTab(tab, elemClass) {
+  let x = document.getElementsByClassName(elemClass);
+  console.log(x)
   for (let i = 0; i < x.length; i++) {
     x[i].style.display = 'none';
   }
   document.getElementById(tab).style.display = 'block';
 }
 
-// function for active tabs for inner accordion mobile view
+// logic for active tabs for inner accordion mobile view
 let accTabsContainer = document.getElementById("tabs");
 let accTab = accTabsContainer.getElementsByClassName("acc-tab");
 for (let i = 0; i < accTab.length; i++) {
@@ -34,27 +34,7 @@ for (let i = 0; i < accTab.length; i++) {
   });
 }
 
-// desktop
-
-// function for switching tabs on desktop view
-function openDeskTab(tabId) {
-  let tabs = document.getElementsByClassName("desk-tab");
-  for (let x = 0; x < tabs.length; x++) {
-    tabs[x].style.display = "none";
-  }
-  document.getElementById(tabId).style.display = "block";
-}
-
-// function for opening and switching tabs inside DeskTap
-function openInnerTab(tabId) {
-  let innerTabs = document.getElementsByClassName("tab-desk");
-  for (let x = 0; x < innerTabs.length; x++) {
-    innerTabs[x].style.display = "none";
-  }
-  document.getElementById(tabId).style.display = "block";
-}
-
-// switching and replacing active class name inside DeskTab
+// switching and replacing active class name inside desktop tab
 let innerTabsContainer = document.getElementById('inner-tab-links-div');
 let tabs = innerTabsContainer.getElementsByClassName('inner-tab-link');
 for (let i = 0; i < tabs.length; i++) {
@@ -66,7 +46,7 @@ for (let i = 0; i < tabs.length; i++) {
   });
 }
 
-// switching and replacing active class name for DeskTab
+// switching and replacing active class name for parent desktop tab
 let deskTabsContainer = document.getElementById('tab-links-div');
 let deskTabs = deskTabsContainer.getElementsByClassName('tab-link');
 for (let i = 0; i < deskTabs.length; i++) {
